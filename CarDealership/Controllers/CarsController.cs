221d9ddea8c2486f5ddb8575a2d6a29d8ac4c835
Vera Weikel and Zach Krogh <vera@>
2019-10-08 09:11:24 -0700

    implement shared layout and setup AddCar route

--- CarDealership/Controllers/CarsController.cs
+++ CarDealership/Controllers/CarsController.cs
@@ -1,3 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using CarDealership.Models;
 using System.Collections.Generic;
+
+namespace CarDealership.Controllers
+{
+  public class CarsController : Controller
+  {
+    [HttpGet("/cars")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult AddCar()
+    {
+      return View();
+    }
+    [HttpPost("/")]
+    public ActionResult Create(string makemodel, int price, int miles)
+    {
+      Car myCar = new Car(makemodel, price, miles);
+      return RedirectToAction("Index", "Home");
+    }
+  }
+}
